a492722f9360f6f322f1cf6d9090de7a06a258c3
Guillaume TESSIER <guillaume.tessier@igtp3-mac06.isim.intra>
2019-05-27 15:50:02 +0200

    Implementation du Cell

--- src/maze/Cell.java
+++ src/maze/Cell.java
@@ -1,7 +1,42 @@
 package maze;
 
-import src.graph.*;
+import graph.*;
 
 public class Cell extends Vertex{
+
+	protected TypeCells type;
+	protected int coordX;
+	protected int coordY;
+
+	public Cell(TypeCells type, int coordX, int coordY, String label){
+		super(label);
+		this.type = type;
+		this.coordX = coordX;
+		this.coordY = coordY;
+	}
+
+	public TypeCells getTypeCells(){
+		return this.type;
+	}
+
+	public int getCoordX(){
+		return this.coordX;
+	}
+
+	public int getCoordY(){
+		return this.coordY;
+	}
+
+	public void setTypeCells(TypeCells type){
+		this.type = type;
+	}
+
+	public void setCoordX(int coordX){
+		this.coordX = coordX;
+	}
+
+	public void setCoordY(int coordY){
+		this.coordY = coordY;
+	}
 
 }
