afe5f9c6002c42553d7cf46424099b0430664184
LucasPln <lucas.paulin@hotmail.fr>
2019-05-27 15:34:37 +0200

    Ajout enum TypeCells & class Cell

---
+++ src/maze/Cell.java
@@ -0,0 +1,7 @@
+package maze;
+
+import src.graph.*;
+
+public class Cell extends Vertex{
+
+}
---
+++ src/maze/TypeCells.java
@@ -0,0 +1,7 @@
+package maze;
+
+import src.graph.*;
+
+public enum TypeCells {
+    DBox, ABox, EBox, WBox;
+}
